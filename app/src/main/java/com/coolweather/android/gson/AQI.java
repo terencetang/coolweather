fa4bcd2c4068b556d67032e940982cf1968ea917
tj <pstangjie@163.com>
2016-12-23 10:55:46 +0800

    zhuanyi01

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2016/12/21 0021.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
